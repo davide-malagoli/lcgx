dd9c3308edf432814b98a10bd2dc3d37895c0151
darkice01 <darkiceelectric.spam@gmail.com>
2014-09-26 19:39:38 +0200

    added custom destination

--- www/js/geolocation.js
+++ www/js/geolocation.js
@@ -1,10 +1,21 @@
-var location;
+var loc;
 var geo;
 
-function updateLocation(pos){
+/*function u(pos){
     location=pos;
-};
+};*/
 
 function initGeo(){
-    //geo= intel.xdk.geolocation.watchPosition(updateLocation);
+    geo= intel.xdk.geolocation.watchPosition(function(pos){
+        //location=pos;
+        //alert(pos);
+        loc=pos;
+        //$("#pos").html(loc.coords.latitude);
+        //alert("c");
+        //alert(loc);
+    },function(){
+        $("#pos").html("error");
+    },
+    {enableHighAccuracy:true}                                        
+    );
 };
